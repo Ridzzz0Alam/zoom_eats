74dcdf63a61d9faf38da06b0df5354f51aa3a442
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-03-31 11:03:55 +0200

    added auth

--- backend/auth/dist/controller/auth.js
+++ backend/auth/dist/controller/auth.js
@@ -1,10 +1,19 @@
 import User from "../model/User.js";
+import jwt from "jsonwebtoken";
 export const loginUser = async (req, res) => {
     try {
         const { email, name, picture } = req.body;
         let user = await User.findOne({ email });
         if (!user) {
+            user = await User.create({
+                name,
+                email,
+                image: picture,
+            });
         }
+        const token = jwt.sign({ user }, process.env.JWT_SEC, {
+            expiresIn: "15d",
+        });
     }
     catch (error) {
     }
--- backend/auth/src/controller/auth.ts
+++ backend/auth/src/controller/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../model/User.js";
+import jwt from "jsonwebtoken";
 
 export const loginUser = async(req:Request, res: Response) => {
     try{
@@ -8,9 +9,16 @@ export const loginUser = async(req:Request, res: Response) => {
         let user = await User.findOne({email});
         
         if(!user){
-            
+            user = await User.create({
+                name,
+                email,
+                image:picture,
+            });
         }
 
+        const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+            expiresIn: "15d",
+        });
     }catch(error){
 
     }
