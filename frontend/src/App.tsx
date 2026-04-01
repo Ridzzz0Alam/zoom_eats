249b21fed816c18732d8dbb47a33d00ef951d23a
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-01 13:51:39 +0200

    starting the login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+
 
 
 const App = () => {
   return (
-    <div className="text-red-400">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
