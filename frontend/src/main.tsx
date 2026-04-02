d0b0d771a74bdee0701f57578119c90af918c193
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-02 11:47:23 +0200

    added public route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AppProvider } from './context/AppContext.tsx';
 
 export const authService = "http://localhost:5000"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="1018188264453-5a7spcjphcgngp306aik9da6g9ut5qc3.apps.googleusercontent.com">
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </GoogleOAuthProvider>;
   </StrictMode>,
 )
