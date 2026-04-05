8469fb3e521e11c98530606a98847b04c0c8b8d4
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-05 12:28:16 +0200

    added states

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,6 +1,7 @@
-import { useLocation, useSearchParams } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import { useAppData } from "../context/AppContext"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CgShoppingCart } from "react-icons/cg";
 
 const Navbar = () => {
     const { isAuth } = useAppData();
@@ -11,8 +12,39 @@ const Navbar = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [search, setSearch] = useState(searchParams.get("search") || "");
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if(search){
+                setSearchParams({search});
+            }else{
+                setSearchParams({});
+            }
+        }, 400);
+
+        return () => clearTimeout(timer);
+    }, [search]);
+
   return (
-    <div>Navbar</div>
+    <div className="w-full bg-white shadow-sm">
+        <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
+            <Link
+            to={"/"}
+            className="text-2xl font-bold text-[#E23744] cursor-pointer"
+            >
+                Zoomato            
+            </Link>
+
+            <div className="flex items-center gap-4">
+                <Link to={"/cart"} className="relative">
+                    <CgShoppingCart className="h-6 w-6 text-[#E23744]"/>
+                    <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center 
+                    justify-center rounded-full bg-[#E23744] text-xs font-semibold text-white">
+                        0
+                    </span>
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
