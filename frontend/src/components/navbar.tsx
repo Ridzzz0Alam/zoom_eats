8b599f30686ac2038ee9839cb74e4ae489f9c2a1
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-05 12:58:55 +0200

    Completed the search navbar functionality

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import { Link, useLocation, useSearchParams } from "react-router-dom";
 import { useAppData } from "../context/AppContext"
 import { useEffect, useState } from "react";
 import { CgShoppingCart } from "react-icons/cg";
-import { BiMapPin } from "react-icons/bi";
+import { BiMapPin, BiSearch } from "react-icons/bi";
 
 const Navbar = () => {
     const { isAuth } = useAppData();
@@ -62,7 +62,18 @@ const Navbar = () => {
                 <div className="mx-auto flex max-w-7xl items-center rounded-lg border shadow-sm">
                     <div className="flex items-center gap-2 px-3 border-r text-gray-700">
                         <BiMapPin className="h-4 w-4 text-[#E23744]"/>
+                        <span className="text-sm truncate max-w-35">city</span>
                     </div>
+                    <div className="flex flex-1 items-center gap-2 px-3">
+                        <BiSearch className="h-4 w-4 text-gray-400"/>
+                        <input
+                            type="text"
+                            placeholder="Search for restaurant"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="w-full py-2 text-sm outline-none"
+                        />
+                    </div>      
                 </div>
             </div>
         )}
