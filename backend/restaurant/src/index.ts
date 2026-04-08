05bbda39193421e3653e6f5a6a0f6bab07a4016d
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-08 21:41:34 +0200

    added config and db files

--- backend/restaurant/src/index.ts
+++ backend/restaurant/src/index.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import connectDB from './config/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
-app.listen()
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`Auth Service is running on port ${PORT}`);
+    connectDB();
+});
