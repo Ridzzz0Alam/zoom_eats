cb8a021f052ffac219446ae89476d71c4a322606
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-11 18:23:20 +0200

    fixed isAuth bug

--- backend/restaurant/src/middlewares/isAuth.ts
+++ backend/restaurant/src/middlewares/isAuth.ts
@@ -1,6 +1,14 @@
 import {Request, Response, NextFunction} from 'express'
 import jwt, {JwtPayload} from 'jsonwebtoken'
-import { IUser } from '../model/User.js'
+
+export interface IUser{
+    _id: string;
+    name: string;
+    email: string;
+    image: string;
+    role: string;
+}
+
 
 export interface AuthenticatedRequest extends Request{
     user?: IUser | null;
